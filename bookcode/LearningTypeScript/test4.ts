a9752ae09ac898c55b62b76421a2109df6c8e2d9
liuhuajian <liuhuajian@csii.com.cn>
2018-09-05 22:33:18 +0800

    Default Changelist

---
+++ bookcode/LearningTypeScript/test4.ts
@@ -0,0 +1,17 @@
+class Person {
+    public name: string;
+    public surname: string;
+    public email: string;
+
+    constructor(name: string, surname: string, email: string) {
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+
+    }
+
+    greet(){
+        alert('hi')
+    }
+
+}
